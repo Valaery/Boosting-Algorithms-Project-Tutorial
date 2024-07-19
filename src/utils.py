6a0d7b3bc604811bda5ed3136f9c253352b5853a
Joan Valaery <joanalcarazrego@gmail.com>
2024-07-19 18:55:42 +0200

    boosting completed

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,31 @@
-from dotenv import load_dotenv
-from sqlalchemy import create_engine
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, roc_auc_score
+from imblearn.metrics import specificity_score
 import pandas as pd
 
-# load the .env file variables
-load_dotenv()
-
-
-def db_connect():
-    import os
-    engine = create_engine(os.getenv('DATABASE_URL'))
-    engine.connect()
-    return engine
+def get_metrics(y_train, y_test, y_pred_train, y_pred_test):
+    # Calcular métricas para el conjunto de entrenamiento
+    train_accuracy = accuracy_score(y_train, y_pred_train)
+    train_f1 = f1_score(y_train, y_pred_train)
+    train_auc = roc_auc_score(y_train, y_pred_train)
+    train_precision = precision_score(y_train, y_pred_train)
+    train_recall = recall_score(y_train, y_pred_train)
+    train_specificity = specificity_score(y_train, y_pred_train)
+    # Calcular métricas para el conjunto de prueba
+    test_accuracy = accuracy_score(y_test, y_pred_test)
+    test_f1 = f1_score(y_test, y_pred_test)
+    test_auc = roc_auc_score(y_test, y_pred_test)
+    test_precision = precision_score(y_test, y_pred_test)
+    test_recall = recall_score(y_test, y_pred_test)
+    test_specificity = specificity_score(y_test, y_pred_test)
+    # Calcular la diferencia entre métricas de entrenamiento y prueba
+    diff_accuracy = train_accuracy - test_accuracy
+    diff_f1 = train_f1 - test_f1
+    diff_auc = train_auc - test_auc
+    diff_precision = train_precision - test_precision
+    diff_recall = train_recall - test_recall
+    diff_specificity = train_specificity - test_specificity
+    # Crear un DataFrame con los resultados
+    metrics_df = pd.DataFrame([[train_accuracy, train_f1, train_auc, train_precision, train_recall, train_specificity],[test_accuracy, test_f1, test_auc, test_precision, test_recall, test_specificity],[diff_accuracy, diff_f1, diff_auc, diff_precision, diff_recall, diff_specificity]],
+                              columns = ['Accuracy', 'F1', 'AUC', 'Precision', 'Recall', 'Specificity'],
+                              index = ['Train','Test', 'Diferencia'])
+    return metrics_df
